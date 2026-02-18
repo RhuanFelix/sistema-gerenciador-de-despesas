dd684ae9df9bcf10a707ae48854cd7b38e5ae366
Rhuan Felix <rhuansantosfelix@gmail.com>
2026-02-17 21:17:30 -0300

    feat: adiciona a configuração do Rate Limiter

--- src/main/java/com/tsi/despesas/configs/RateLimiterConfiguration.java
+++ src/main/java/com/tsi/despesas/configs/RateLimiterConfiguration.java
@@ -1,4 +1,33 @@
 package com.tsi.despesas.configs;
 
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.time.Duration;
+
+@Configuration
 public class RateLimiterConfiguration {
+
+    @Bean
+    public RateLimiter categoriaRateLimiter() {
+        RateLimiterConfig config = RateLimiterConfig.custom()
+                .limitForPeriod(5)
+                .limitRefreshPeriod(Duration.ofSeconds(20))
+                .timeoutDuration(Duration.ofMillis(500))
+                .build();
+
+        return RateLimiter.of("categoriaRateLimiter", config);
+    }
+
+    @Bean
+    public RateLimiter despesaRateLimiter() {
+        RateLimiterConfig config = RateLimiterConfig.custom()
+                .limitForPeriod(10)
+                .limitRefreshPeriod(Duration.ofSeconds(15))
+                .timeoutDuration(Duration.ofMillis(500))
+                .build();
+
+        return RateLimiter.of("despesaRateLimiter", config);
+    }
 }
